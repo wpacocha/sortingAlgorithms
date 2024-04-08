9e247b99f4676d37f455274601c4baef0a7fb87d
wpacocha <77246645+wpacocha@users.noreply.github.com>
2024-04-08 07:44:11 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,43 +6,65 @@
 #include <chrono>
 
 using namespace std::chrono;
+
 using namespace std;
 
-struct movie {
+struct movie{
     string title;
     float rating;
 };
 
-void quickSort(vector<movie>& arr, int low, int high) {
-    if (low >= high)
-        return;
+void merge(vector<movie>& tab, int left, int mid, int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-    float pivot = arr[(low + high) / 2].rating;
-    int i = low;
-    int j = high;
+    //tworzenie tablic pom
+    vector<movie> L(n1), R(n2);
 
-    while (i <= j) {
-        while (arr[i].rating < pivot) {
+    //kopiowanie danych do tablic pom
+    for (int i =0;i<n1;i++) L[i] = tab[left + i];
+    for (int i=0;i<n2;i++) R[i] = tab[mid+1+i];
+
+    //scalanie tab pom
+    int i=0,j=0,k=left;
+    while(i<n1&&j<n2){
+        if(L[i].rating<=R[j].rating){
+            tab[k] = L[i];
             i++;
+        } else {
+            tab[k] = R[j];
+            j++;
         }
-        while (arr[j].rating > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(arr[i], arr[j]);
-            i++;
-            j--;
-        }
+        k++;
     }
 
-    quickSort(arr, low, j);
-    quickSort(arr, i, high);
-}
+    //kopiowanie pozostalych elem l[]
+    while(i<n1){
+        tab[k] = L[i];
+        i++;
+        k++;
+    }
 
+    //kopiowanie pozostalych elem r[]
+    while(i<n2){
+        tab[k] = R[j];
+        j++;
+        k++;
+    }
+};
+
+void mergeSort(vector<movie>& tab, int left, int right){
+    if(left<right) {
+        //znalezienie srodka
+        int mid = left + (right-left)/2;
+        mergeSort(tab,left,mid);
+        mergeSort(tab,mid+1,right);
+        merge(tab,left,mid,right);
+    }}
 
 void printArray(const vector<movie>& tab){
-    for(const auto& movie : tab) {
-        cout<<movie.title<<": "<<movie.rating<<endl;
+    for (const auto& movie : tab) {
+        cout<< movie.title << ": "<<movie.rating<<endl;
     }
     cout<<endl;
 }
@@ -86,14 +108,13 @@ vector<movie> readMoviesFromFile(const string& filename, int num_movies, int max
 
 int main() {
     string path = "/Users/wpacocha/Studia/Semestr IV/PAA/MergeSort/dane.csv";
-    int max = 962902;
+    int max = 962903;
     vector<int> data_sizes = {10000,100000,500000,750000,max};
-
     for (int size : data_sizes) {
         auto data = readMoviesFromFile(path,size,max);
 
         auto start = high_resolution_clock::now();
-        quickSort(data,0,data.size() - 1);
+        mergeSort(data,0,data.size() - 1);
         auto stop = high_resolution_clock::now();
 
         auto duration = duration_cast<milliseconds>(stop - start);
@@ -102,3 +123,4 @@ int main() {
     }
     return 0;
 }
+
